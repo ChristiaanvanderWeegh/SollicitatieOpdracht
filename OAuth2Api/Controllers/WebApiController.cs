30b4597b88d9097b292947187b191ee5abc56c71
christiaan.vd.weegh <christiaan.van.der.weegh@topicus.nl>
2020-12-08 10:43:37 +0100

    Setup for OAuth2Api

---
+++ OAuth2Api/Controllers/WebApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace OAuth2Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    [Authorize]
+    public class WebApiController : Controller
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello REST API", "I am Authorized" };
+        }
+
+        public string Get(int id)
+        {
+            return "Hello Authorized API with ID = " + id;
+        }
+
+        public void Post([FromBody] string value)
+        {
+        }
+
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        public void Delete(int id)
+        {
+        }
+    }
+}
